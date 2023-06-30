8f7c70171cad4aafb83236740a50da0974fb9bd4
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2023-06-30 17:09:29 +0900

    Add game controller

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,25 @@
+class GameController < ApplicationController
+  def list
+    render :json => {}
+  end
+
+  def new
+    render :json => {}
+  end
+
+  def join
+    render :json => {}
+  end
+
+  def move
+    render :json => {}
+  end
+
+  def board
+    render :json => {}
+  end
+
+  def giveup
+    render :json => {}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'game.list', to: 'game#list'
+  get 'game.new', to: 'game#new'
+  post 'game.join', to: 'game#join'
+  post 'game.move', to: 'game#move'
+  get 'game.board', to: 'game#board'
+  post 'game.giveup', to: 'game#giveup'
 end
